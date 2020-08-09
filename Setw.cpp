49604caa51b064cde74e54350ceec4bd549a7ee0
Stanislav <68480460+Sony2020-cout@users.noreply.github.com>
2020-08-09 22:11:25 +0300

    Add files via upload

---
+++ Setw.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::setw; 
+using std::endl;
+
+int main()
+{
+	setlocale(0, "ru");
+	long pop1 = 2365464, pop2 = 58, pop3 = 8457;
+	cout << setw(6) << "Город " << setw(15)
+		<< "Население " << endl
+		<< setw(6) << "Москва " << setw(12) << pop1 << endl
+		<< setw(6) << "Киров " << setw(13) << pop2 << endl
+		<< setw(7) << "Угрюмовка " << setw(9) << pop3 << endl;
+	return 0;
+}
